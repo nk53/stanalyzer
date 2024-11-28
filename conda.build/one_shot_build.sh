732de1cb5338f266aa6b6a59a2993094026dcfa6
Nathan Kern <natek5309@gmail.com>
2024-11-28 12:29:48 -0600

    fix malformed commands

--- conda.build/one_shot_build.sh
+++ conda.build/one_shot_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+conda init
 conda activate base
-conda install -y build
+conda install -y conda-build
 conda update -y conda conda-build
 
 git clone "https://github.com/nk53/stanalyzer" sta-build
