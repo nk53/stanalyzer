4e7d752d3e4427ccc1505aca846e50db851afb94
Nathan Kern <natek5309@gmail.com>
2024-11-28 12:21:53 -0600

    Added a one-size-fits-all build script

---
+++ conda.build/one_shot_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+conda activate base
+conda install -y build
+conda update -y conda conda-build
+
+git clone "https://github.com/nk53/stanalyzer" sta-build
+cd sta-build/conda.build
+./local_make.sh
+cd ../..
+rm -r sta-build
