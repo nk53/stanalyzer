d0cca25f438cc862873a4f0aa2ed001e9f53d90f
Nathan Kern <natek5309@gmail.com>
2024-11-28 12:43:24 -0600

    conda init in script differs from interactive shell init

--- conda.build/one_shot_build.sh
+++ conda.build/one_shot_build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 set -e
-conda init
+
+# unset previous conda vars
+unset CONDA_SHLVL
+unset _CE_CONDA
+unset _CE_M
+unset CONDA_EXE
+
+# initialize conda
+eval "$(python -m conda shell.bash hook)"
+
+# ensure conda and conda-build are up-to-date
 conda activate base
 conda install -y conda-build
 conda update -y conda conda-build
 
+# download repo and run build
 git clone "https://github.com/nk53/stanalyzer" sta-build
 cd sta-build/conda.build
 ./local_make.sh
 cd ../..
+
+# remove repo
 rm -r sta-build
