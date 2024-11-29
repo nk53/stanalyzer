43ffc433188a61d6cfba69fcff007b4bb7f76076
Nathan Kern <natek5309@gmail.com>
2024-11-29 15:29:58 -0600

    Fix attempt for build script issues

--- conda.build/local_make.sh
+++ conda.build/local_make.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-output_folder=$HOME/Downloads/stanalyzer-builds
-output_archive=$output_folder.tgz
+output_dir=stanalyzer-builds
+output_dir_parent="$HOME/Downloads"
+output_abspath="$output_dir_parent/$output_dir"
+output_archive="$output_dir.tgz"
 
-if [ ! -e "$output_folder" ]; then
-    mkdir -p "$output_folder"
+if [ ! -e "$output_abspath" ]; then
+    mkdir -p "$output_abspath"
 fi
 
 CHANNEL="-c conda-forge"
-if [ "$1" == "-f" ]; then
-    OPTS="--keep-going"
-else
-    OPTS="--skip-existing"
-fi
-OPTS="$OPTS --output-folder \"$output_folder\" --no-anaconda-upload"
+OPTS="--keep-going --no-anaconda-upload"
 
 set -e
 
-conda build --variants '{suffix: ["", client]}' $OPTS $CHANNEL .
-conda build --no-remove-work-dir --variants "{suffix: [dev]}" $OPTS $CHANNEL .
+conda build --variants '{suffix: ["", client]}' \
+    --output-folder "$output_abspath" $OPTS $CHANNEL .
+conda build --no-remove-work-dir --variants "{suffix: [dev]}" \
+    --output-folder "$output_abspath" $OPTS $CHANNEL .
 
 echo "cleaning up intermediate files ..."
 conda build purge
 
 echo "build complete, compressing ..."
-tar cvzf "$output_archive" "$output_folder"
-rm -r "$output_folder"
+cd "$output_dir_parent"
+tar cvzf "$output_archive" "$output_dir"
+rm -r "$output_dir"
 
-echo "final output: $output_archive"
+echo "final output: $output_dir_parent/$output_archive"
--- conda.build/one_shot_build.sh
+++ conda.build/one_shot_build.sh
@@ -7,19 +7,70 @@ unset _CE_CONDA
 unset _CE_M
 unset CONDA_EXE
 
+# does this shell have conda in its PATH?
+echo "Trying to find conda ..."
+if ! installer=$(which conda); then
+    # does the default python have a conda module?
+    if python -m conda -h; then
+        installer="python -m conda"
+    fi
+fi >/dev/null 2>/dev/null
+
+if [ -z "$installer" ]; then
+    echo "Can't find conda executable."
+    echo "Please enter path to conda executable, or leave the line blank and hit Enter to cancel."
+    prompt="Path to conda executable (default: quit): "
+    while [ 1 ]; do
+        read -p "$prompt" installer
+        [ -z "$installer" ] && exit 1
+        if [ ! -e "$installer" ]; then
+            echo "Sorry, '$installer' does not exist. Try again or hit Enter to cancel."
+        elif [ ! -f "$installer" ]; then
+            echo "Sorry, '$installer' is not a regular file. Try again or hit Enter to cancel."
+        elif [ ! -x "$installer" ]; then
+            echo "Sorry, '$installer' is not executable. Try again or hit Enter to cancel."
+        else
+            # got an actual program, at least
+            break
+        fi
+    done
+fi
+
 # initialize conda
-eval "$(python -m conda shell.bash hook)"
+echo "found '$installer'. Trying to initialize ..."
+eval "$($installer 'shell.bash' 'hook')"
 
 # ensure conda and conda-build are up-to-date
-conda activate base
+echo "Checking that conda and conda-build are up to date ..."
 conda install -y conda-build
 conda update -y conda conda-build
 
 # download repo and run build
-git clone "https://github.com/nk53/stanalyzer" sta-build
-cd sta-build/conda.build
+orig_dir=sta-dir
+clone_dir=$orig_dir
+if [ -e "$clone_dir" ]; then
+    echo "Can't clone into '$clone_dir' because it already exists."
+    echo "Please remove '$clone_dir' or enter a new path below."
+    echo "Enter a blank line to cancel."
+    while [ 1 ]; do
+        read -p "Path to save stanalyzer source: " clone_dir
+        [ -z "$clone_dir" ] && exit 1
+        if [ "$clone_dir" == "$orig_dir" ]; then
+            echo "Please remove '$orig_dir' and try again."
+            exit 1
+        elif [ -e "$clone_dir" ]; then
+            echo "$clone dir already exists. Please try again or hit Enter to cancel."
+        else
+            break
+        fi
+    done
+fi
+
+git clone "https://github.com/nk53/stanalyzer" "$clone_dir"
+cd "$clone_dir"/conda.build
 ./local_make.sh
 cd ../..
 
 # remove repo
-rm -r sta-build
+chmod -R 777 "$clone_dir"
+rm -r "$clone_dir"
