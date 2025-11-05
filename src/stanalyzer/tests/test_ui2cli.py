1d74291916da8d52b9b4c9f08671656186258926
Nathan Kern <natek5309@gmail.com>
2025-11-05 14:12:52 +0900

    note to self: unit testing will temporarily reduce insanity

---
+++ src/stanalyzer/tests/test_ui2cli.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+# TODO: the idea is to parse options in static/analysis.yml and check that
+# the corresponding analysis program is able to accept the option
+# IDK whether this just should be done by actually running a short analysis
